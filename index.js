36e53417d0c224e9edc545051db0a34779326970
Abrar Sardar <abrarsardar8955@gmail.com>
2023-01-25 20:16:43 +0500

    Backend Stripe Complete

--- index.js
+++ index.js
@@ -8,28 +8,33 @@ app.use(express.json())
 
 app.post('/payment-sheet', async (req, res) => {
   // Use an existing Customer ID if this is a returning customer.
-  const customer = await stripe.customers.create();
-  const ephemeralKey = await stripe.ephemeralKeys.create(
-    {customer: customer.id},
-    {apiVersion: '2022-11-15'}
-  );
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: 1099,
-    currency: 'eur',
-    customer: customer.id,
-    automatic_payment_methods: {
-      enabled: true,
-    },
-  });
+  try {
+    const customer = await stripe.customers.create();
+    const ephemeralKey = await stripe.ephemeralKeys.create(
+      { customer: customer.id },
+      { apiVersion: '2022-11-15' }
+    );
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: 1099,
+      currency: 'eur',
+      customer: customer.id,
+      automatic_payment_methods: {
+        enabled: true,
+      },
+    });
 
-  res.json({
-    paymentIntent: paymentIntent.client_secret,
-    ephemeralKey: ephemeralKey.secret,
-    customer: customer.id,
-    publishableKey: 'pk_test_51MSiMCLutRiIN166MnCfrWJPJSxxK5PhrLHKSYh6WaTKV4oiIm2myQGPluomaofSMkxg1d4J9u2k0iMXefh0RnV600Dds5Mgo3'
-  });
+    res.json({
+      paymentIntent: paymentIntent.client_secret,
+      ephemeralKey: ephemeralKey.secret,
+      customer: customer.id,
+      publishableKey: 'pk_test_51MSiMCLutRiIN166MnCfrWJPJSxxK5PhrLHKSYh6WaTKV4oiIm2myQGPluomaofSMkxg1d4J9u2k0iMXefh0RnV600Dds5Mgo3'
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
 });
 
-app.listen(port,()=>{
+app.listen(port, () => {
   console.log('server running')
 })
